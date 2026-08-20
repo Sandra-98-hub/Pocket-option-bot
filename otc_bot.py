16e5749652b6661be7362ca318f1d57a8489196d
Sandra-98-hub <sandypierre98@gmail.com>
2026-08-20 01:04:43 -0400

    Refactor otc_bot.py to simplify output
    Removed OTC market definitions and related print statements.

--- otc_bot.py
+++ otc_bot.py
@@ -1,15 +1,3 @@
-# Pocket Option OTC signal bot
-# Markets: EUR/USD OTC, AUD/CAD OTC, AED/CNY OTC, AUD/NZD OTC
+import pocket_option
 
-OTC_MARKETS = [
-    "EURUSD_otc",
-    "AUDCAD_otc",
-    "AEDCNY_otc",
-    "AUDNZD_otc",
-]
-
-print("Pocket Option OTC bot starting...")
-print("OTC markets loaded:")
-for market in OTC_MARKETS:
-    print(market)
-print("Waiting for OTC market data...")
+print("POCKET OPTION PACKAGE OK")
